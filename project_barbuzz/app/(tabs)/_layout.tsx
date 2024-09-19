7399f78ef93cbbd9f903146a7f22c1e30e44a29e
calyskew <130619078+calyskew@users.noreply.github.com>
2024-09-19 13:21:36 -0400

    stats edits

--- project_barbuzz/app/(tabs)/_layout.tsx
+++ project_barbuzz/app/(tabs)/_layout.tsx
@@ -35,7 +35,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="new"
         options={{
-          title: 'Profile',
+          title: 'Personal Stats',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
           ),
