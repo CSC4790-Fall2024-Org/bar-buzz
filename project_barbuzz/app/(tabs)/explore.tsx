d48af8cebc4f17d3ccd84891cbebbc52f794297f
calyskew <130619078+calyskew@users.noreply.github.com>
2024-09-23 11:01:15 -0400

    commit

--- project_barbuzz/app/(tabs)/explore.tsx
+++ project_barbuzz/app/(tabs)/explore.tsx
@@ -6,7 +6,7 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import { FlatList } from 'react-native-gesture-handler';
 
 
