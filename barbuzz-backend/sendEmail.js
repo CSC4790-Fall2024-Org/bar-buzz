fcce45314c5c3904d3f201ebbb98161a153a4ca5
navisingh2 <navisinghk2@gmail.com>
2025-03-24 20:32:29 -0400

    Update sendEmail.js

--- barbuzz-backend/sendEmail.js
+++ barbuzz-backend/sendEmail.js
@@ -1,9 +1,12 @@
 // sendEmail.js
-const mailjet = require('./mailjet');
+const mailjet = require('node-mailjet').connect(
+  process.env.MJ_APIKEY_PUBLIC,
+  process.env.MJ_APIKEY_PRIVATE
+);
 
 async function sendVerificationEmail(toEmail, verificationLink) {
   // This is the structure required by Mailjet's v3.1 API
-  const request = mailjet.post('send', { version: 'v3.1' }).request({
+  const requestBody = {
     Messages: [
       {
         From: {
@@ -21,7 +24,14 @@ async function sendVerificationEmail(toEmail, verificationLink) {
         `,
       },
     ],
-  });
+  };
+
+    // 2) Pass requestBody to mailjet
+    const response = await mailjet
+    .post('send', { version: 'v3.1' })
+    .request(requestBody);
+
+  console.log('Mailjet response:', response.body);
 
   try {
     const response = await mailjet.post('send', { version: 'v3.1' }).request(requestBody);
@@ -31,6 +41,8 @@ async function sendVerificationEmail(toEmail, verificationLink) {
     console.error('Error sending verification email:', error);
     throw error;
   }
+
+
 }
 
 module.exports = { sendVerificationEmail };
